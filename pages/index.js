48a63364041b41d4669a0d43364818b693400652
toydev <toy@ownway.info>
2022-06-29 10:19:32 +0900

    mui で GUI を整えた。

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,52 @@
 import { useRouter } from 'next/router'
+import LanguageIcon from '@mui/icons-material/Language'
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 import ClientSessionManager from 'webrtc/client/ClientSessionManager'
 
-export default function Home() {
+const theme = createTheme()
+
+export default function Page() {
   const router = useRouter()
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    router.push(`/session/${ClientSessionManager.getNewSessionId()}`)
+  }
+
   return (
-    <div>
-      <button onClick={() => router.push(`/session/${ClientSessionManager.getNewSessionId()}`)}>開始</button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LanguageIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Start
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}>
+              セッションを開始する
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
--- pages/session/[sessionId].js
+++ pages/session/[sessionId].js
@@ -1,24 +1,43 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useImmer } from 'use-immer'
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import LanguageIcon from '@mui/icons-material/Language'
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import ListItem from '@mui/material/ListItem'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import ListItemText from '@mui/material/ListItemText'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 import ClientSessionManager from 'webrtc/client/ClientSessionManager'
 
+const theme = createTheme()
+
 export default function Home() {
+  const inputMessageTextField = useRef()
   const router = useRouter()
   const { sessionId } = router.query
-  const [inputMessage, setInputMessage] = useState("")
   const [connected, setConnected] = useState(false)
   const [messages, setMessages] = useImmer([])
 
   const receiveMessage = (message) => {
-    setMessages(messages => setMessages(messages => { messages.push(`Other: ${message}`); return messages }))
+    setMessages(messages => { messages.push({ type: 'receive', message: message }); return messages })
   }
 
-  const sendMessage = (sessionId, message) => {
-    if (ClientSessionManager.getSession(sessionId).send(message)) {
-      setMessages(messages => setMessages(messages => { messages.push(`You: ${message}`); return messages }))
-      setInputMessage("")
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const message = inputMessageTextField.current.value
+    if (message !== "" && ClientSessionManager.getSession(sessionId).send(message)) {
+      console.log(messages)
+      setMessages(messages => { messages.push({ type: 'send', message: message }); return messages })
+      inputMessageTextField.current.value = ""
     }
   }
 
@@ -37,12 +56,54 @@ export default function Home() {
   }, [sessionId])
 
   return (
-    <div>
-      <input type="text" value={inputMessage} onChange={e => setInputMessage(e.target.value)} />
-      <button onClick={() => sendMessage(sessionId, inputMessage)} disabled={!connected}>送信</button>
-      <ul>
-        {messages.map((i, index) => <li key={index.toString()}>{i}</li>)}
-      </ul>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LanguageIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Session
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              inputRef={inputMessageTextField}
+              margin="normal"
+              required
+              fullWidth
+              id="message"
+              label="Message"
+              name="message"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={!connected}>
+              話す
+            </Button>
+            {messages.slice(-6).map((i, index) =>
+              <ListItem key={index}>
+                <ListItemAvatar>
+                  <Avatar>
+                    {i.type == "send" ? <ArrowBackIcon /> : <ArrowForwardIcon />}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={i.message} />
+              </ListItem>
+            )}
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
